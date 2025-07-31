e0b4fe72e584c4a5b1ccc75813ba447e34c386fd
fadhlihilmanss <fadhli.hilmanss@gmail.com>
2025-07-31 20:56:37 +0700

    push

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -44,84 +44,9 @@
 
   {{-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script> --}}
   <script type="text/javascript" src="/assets/toastify/toastify.min.js"></script>
+ 
   <!-- toast -->
-  <script>
-        document.addEventListener('livewire:init', () => {
-            Livewire.on('success-message', (event) => {
-                Toastify({
-                    text: "✔️ " + event, // Tambah ikon check
-                    className: "info",
-                    duration: 3000,
-                    newWindow: true,
-                    close: true,
-                    gravity: "top",
-                    style: {
-                        background: "#4de265",
-                        borderRadius: "8px",
-                        fontWeight: "bold",
-                        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
-                        color: "#fff"
-                    }
-                }).showToast();
-            });
-
-            Livewire.on('failed-message', (event) => {
-                Toastify({
-                    text: "❌ " + event,
-                    className: "info",
-                    duration: 3000,
-                    newWindow: true,
-                    close: true,
-                    gravity: "top",
-                    style: {
-                        background: "#e53935",
-                        borderRadius: "8px",
-                        fontWeight: "bold",
-                        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
-                        color: "#fff"
-                    }
-                }).showToast();
-            });
-        });
-  </script>
-  @if (session('success-message'))
-        <script>
-            Toastify({
-                text: "✔️ {{ session('success-message') }}",
-                className: "info",
-                duration: 3000,
-                newWindow: true,
-                close: true,
-                gravity: "top",
-                style: {
-                    background: "#4de265",
-                    borderRadius: "8px",
-                    fontWeight: "bold",
-                    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
-                    color: "#fff"
-                }
-            }).showToast();
-        </script>
-  @endif 
-  @if (session('failed-message'))
-        <script>
-            Toastify({
-                text: "❌ {{ session('failed-message') }}",
-                className: "info",
-                duration: 3000,
-                newWindow: true,
-                close: true,
-                gravity: "top",
-                style: {
-                    background: "#e53935",
-                    borderRadius: "8px",
-                    fontWeight: "bold",
-                    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
-                    color: "#fff"
-                }
-            }).showToast();
-        </script>
-  @endif
+  @include('components.layouts.toast')
 
   <!-- Page Specific JS File -->
 </body>
